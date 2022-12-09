203a97c8750d59efeab3f1d4d5ceee823e9c2503
oseiderick <oseiderick41@gmail.com>
2022-12-09 15:34:56 -0800

    ALhpabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	/*prints out the new line*/
+	putchar('\n');
+
+	return (0);
+
+
+}
