c3858b84c7ee7305b41e5cd26ace1b0d4ded68b8
oseiderick <oseiderick41@gmail.com>
2022-12-08 21:01:48 -0800

    Displaying contents

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main-Entry point for the the program
+ *
+ * return- prints out zero when the program fails
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	puts("/"Programming is like building a multilingual puzzle");
+return(0);
+}
