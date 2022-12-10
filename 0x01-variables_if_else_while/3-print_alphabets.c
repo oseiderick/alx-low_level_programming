bfa927faf3362285f703f7cff8ac99043980cd6c
oseiderick <oseiderick41@gmail.com>
2022-12-09 17:48:41 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char a, b;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	for (b = 'A'; b <= 'Z'; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
