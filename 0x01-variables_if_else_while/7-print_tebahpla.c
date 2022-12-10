878ae86a86ebee9d16230aa3f2172677772c275f
oseiderick <oseiderick41@gmail.com>
2022-12-09 17:24:03 -0800

    Reverse letters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char p;
+
+	for (p = 'z'; p >= 'a'; p--)
+		putchar(p);
+	putchar('\n');
+	return (0);
+}
