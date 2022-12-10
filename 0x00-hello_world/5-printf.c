d7ee4307f2f7b2f9c61b28cf8442d10e82f71a3b
oseiderick <oseiderick41@gmail.com>
2022-12-10 01:20:41 -0800

    Added Readme

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main: Entry of the code
+ * main - Entry of the code
  *
- * return:Always 0 (success)
+ * Return:Always 0 (success)
  */
 int main(void)
 {
---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry pint
+ *
+ * Return: ALways 0
+ */
+int main(void)
+{
+	int a , b;
+
+	for (a ='0'; a <= '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			putchar(a);
+			putchar(b);
+			
+			if (a < '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
