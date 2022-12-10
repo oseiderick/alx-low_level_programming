c60db6740343b306905e7d370fde61e41e84d836
oseiderick <oseiderick41@gmail.com>
2022-12-09 17:30:05 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Alwarys 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+
+}
