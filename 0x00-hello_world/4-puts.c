65e614f39f771854fbe7ff247e8be5c661efb7e8
oseiderick <oseiderick41@gmail.com>
2022-12-10 01:28:58 -0800

    Using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main-Entry point for the the program
+ * main - Entry point for the the program
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	puts("/""Programming is like building a multilingual puzzle");
+	puts('\n');
 	return (0);
 }
