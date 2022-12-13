f16cf0b4093ce903952d6daefc7c48818819775c
oseiderick <oseiderick41@gmail.com>
2022-12-13 14:21:13 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar('_-);
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	puthcar('\n'');
+	return (0);
+}
