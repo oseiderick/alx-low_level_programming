057458d370c673fd25095a496549bb62559bcb46
oseiderick <oseiderick41@gmail.com>
2022-12-14 04:05:22 -0800

    9times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,23 +7,30 @@
  */
 void times_table(void)
 {
-	int i, mult, result;
+	int r, c, d;
 
-	for (i = 0; i <= 9; i++)
+	for (r = 0; r <= 9; r++)
 	{
-		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
+	_putchar('0');
+	_putchar(',');
+	_putchar(' ');
+		for (c = 1; c = <= 9; c++)
 		{
-			_putchar(',');
-			_putchar(' ');
-			result = i * mult;
-			if (result <= 9)
-				_putchar(' ');
-			else
-				_putchar((result / 10) +'0');
-
-			_putchar((result % 10) +'0');
+		d = ((d / 10) > 0)
+		{
+		_putchar((d / 10) + '0');
 		}
-		_putchar('\n');
-}
+		else
+		{
+		_putchar(' ');
+		}
+		_putchar((d % 10) + '0');
+	if (c < 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+		}
+	}
+}	
+
