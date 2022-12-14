0568a5add3cdcdacd2b4b192555af725674fd8d9
oseiderick <oseiderick41@gmail.com>
2022-12-13 21:11:27 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha  - checks if input is an alphabet
+ * @c: input to test
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c<=90 || (c>= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
