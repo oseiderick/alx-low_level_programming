e29d79f011b96071c2377fdbf32d2e8d756c42ba
oseiderick <oseiderick41@gmail.com>
2022-12-14 20:27:20 -0800

    sign determ.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - print sogn of a number
+ * @n: number input
+ * Return: 1, 0, -1
+ */
+int print_sign(int n)
+{
+	int a = -1;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{	_putchar('-');
+		return ('a');
+	}
+}
