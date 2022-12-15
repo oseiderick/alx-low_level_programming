534499577708f1f2946f1bea93ac4ef2dc9598f4
oseiderick <oseiderick41@gmail.com>
2022-12-14 21:35:23 -0800

    fib

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int fib[52];
+	int i;
+
+	fib[0] = 0;
+	fib[1] = 1;
+	for (i = 2; i <= 51; i++)
+	{
+		fib[i] = fib[i - 1] + fib[i -2];
+		if (i < 51)
+			printf("%ld, ", fib[i]);
+		else
+			printf("%ld\n", fib[i]);
+	}
+	return (0);
+}
