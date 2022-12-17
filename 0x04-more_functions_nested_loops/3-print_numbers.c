e297561846be99ae68511b68ddcadd016666e38e
oseiderick <oseiderick41@gmail.com>
2022-12-16 16:35:52 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: null
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <=9; i++)
+	{
+		putchar(i +'0');
+	}
+	putchar('\n');
+}
