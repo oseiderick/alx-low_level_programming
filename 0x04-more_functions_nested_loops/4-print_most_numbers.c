8f744aecefce097439412594b765a2bc7f687ba9
oseiderick <oseiderick41@gmail.com>
2022-12-16 16:42:29 -0800

    most

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - print most numbers
+ * Return: null
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+			putchar(i +'0');
+	}
+	putchar('\n');
+}
