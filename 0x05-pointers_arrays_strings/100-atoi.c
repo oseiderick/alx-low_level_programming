fe6c9a8200cfb232d05200fb7aee2ede95ab513d
oseiderick <oseiderick41@gmail.com>
2022-12-20 01:22:57 -0800

    string int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _atio - converts string to integer
+ * @s: string
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	int i, neg, start, result;
+	int temp;
+	double res;
+
+	res = 0;
+	neg = 0;
+	start = 5;
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		if (start == 0)
+			break;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			if (start == 5)
+			{
+				start = 1;
+			}
+			temp = s[i] - '0';
+			res = res * 10 +temp;
+		}
+		else if (s[i] == '-')
+			neg += 1;
+		else if (start ==1)
+			start = 0;
+	}
+	if (neg % 2 == 1)
+		res *= -1;
+	result = res;
+	return (result);
+}
