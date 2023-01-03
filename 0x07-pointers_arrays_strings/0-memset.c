234d4a9aebc68d814e9aeb307a74af1a1211631d
oseiderick <oseiderick41@gmail.com>
2023-01-03 02:38:38 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * _memset - Entry of the code
+ *
+ * @s: memory area
+ * @n: number of characters to write to memory
+ * @b: constant byte
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *p = s;
+
+	while (n--)
+		*p++ = (unsigned char) b;
+	return (s);
+
+}
