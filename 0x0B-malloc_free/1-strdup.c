e663bcbc1b3879c77e6eace68c9119b4bf6df433
oseiderick <oseiderick41@gmail.com>
2023-01-10 19:56:57 -0800

    task2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strup - copy
+ * @str: pointer to string
+ * Return: pointer
+ */
+
+char *_strup(char *str)
+{
+	char *s;
+	int i = 0, j;
+
+	if (!str)
+		return (NULL);
+
+	while (*(str + i))
+		i++;
+	i++;
+	s = malloc(sizeof(char) * i);
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (j = 0; j <= i; j++)
+	{
+		s[j] = str[j];
+	}
+	return (s);
+}
